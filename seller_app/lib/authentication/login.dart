a83f425bceac9d5041fe4ba05423e20144de1f56
Harendra-PrajapatiIPHtech <harendra.prajapati@iphtechnologies.com>
2023-04-06 10:45:10 +0530

    commot

--- seller_app/lib/authentication/login.dart
+++ seller_app/lib/authentication/login.dart
@@ -1,8 +1,10 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:seller_app/global/global.dart';
 import 'package:seller_app/widgets/error_Dialog.dart';
 import 'package:seller_app/widgets/loading_dialog.dart';
+import 'package:seller_app/widgets/mainScreens/home_screen.dart';
 
 import '../widgets/custom_text_field.dart';
 
@@ -20,6 +22,7 @@ class _LoginScreenState extends State<LoginScreen> {
   formValidation() {
     if (emailController.text.isNotEmpty && passwordController.text.isNotEmpty) {
 // login
+      loginNow();
     } else {
       showDialog(
           context: context,
@@ -31,34 +34,56 @@ class _LoginScreenState extends State<LoginScreen> {
     }
   }
 
-  // loginNow() async {
-  //   showDialog(
-  //       context: context,
-  //       builder: (c) {
-  //         return const LoadingDialog(
-  //           message: 'Checking Creadential',
-  //         );
-  //       });
-  //   User? currentUser;
-  //   await firebaseAuth
-  //       .signInWithEmailAndPassword(
-  //     email: emailController.text.trim(),
-  //     password: passwordController.text.trim(),
-  //   )
-  //       .then((auth) {
-  //     currentUser = auth.user!;
-  //   }).catchError((error) {
-  //     Navigator.pop(context);
-  //     showDialog(
-  //         context: context,
-  //         builder: (context) {
-  //           return ErrorDialog(
-  //             message: error.message.toString(),
-  //           );
-  //         });
-  //   });
-  //   if (currentUser != null) {}
-  // }
+  loginNow() async {
+    showDialog(
+        context: context,
+        builder: (c) {
+          return const LoadingDialog(
+            message: 'Checking Creadential',
+          );
+        });
+    User? currentUser;
+    await firebaseAuth
+        .signInWithEmailAndPassword(
+      email: emailController.text.trim(),
+      password: passwordController.text.trim(),
+    )
+        .then((auth) {
+      currentUser = auth.user!;
+    }).catchError((error) {
+      Navigator.pop(context);
+      showDialog(
+          context: context,
+          builder: (context) {
+            return ErrorDialog(
+              message: error.message.toString(),
+            );
+          });
+    });
+    if (currentUser != null) {
+      readDataAndSetDataLocally(currentUser!).then((value) {
+        Navigator.pop(context);
+        Navigator.push(context,
+            MaterialPageRoute(builder: (context) => const HomeScreen()));
+      });
+    }
+  }
+
+  Future readDataAndSetDataLocally(User currentUser) async {
+    await FirebaseFirestore.instance
+        .collection("sellers")
+        .doc(currentUser.uid)
+        .get()
+        .then((snapshot) async {
+      await sharedPreferences!.setString("uid", currentUser.uid);
+      await sharedPreferences!
+          .setString("email", snapshot.data()!["sellerEmail"]);
+      await sharedPreferences!
+          .setString("name", snapshot.data()!["sellerName"]);
+      await sharedPreferences!
+          .setString("PhotoUrl", snapshot.data()!["sellerAvtar"]);
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
